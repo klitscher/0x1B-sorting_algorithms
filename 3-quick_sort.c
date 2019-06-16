60349abaa1d13cce1abacb15887925fceab3a5d7
klitscher <672@holbertonschool.com>
2019-06-16 21:44:27 +0000

    Update 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,11 +1,12 @@
 #include "sort.h"
 
 /**
- * quick_sort -
- *
- * Return: 
+ * quick_sort - sorts an array using quick-sort
+ * @array: array to sort
+ * @size: size of array
+ * Return: void
  */
 void quick_sort(int *array, size_t size)
 {
-
+	
 }
